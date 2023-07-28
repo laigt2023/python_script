5aecd964bd119a57ef5fc62a4265ffee9c2d08f1
laigt <171547080@qq.com>
2023-07-28 15:50:36 +0800

    添加自动清空推理文件夹（./target_xml ./compare_xml ）

--- ai_predict_test.py
+++ ai_predict_test.py
@@ -11,24 +11,28 @@ import ai_predict_to_xml as AI_PREDICT
 # 对比模型
 TARGET_AI_TASK_ID='8f97a571-22e5-41e3-afed-8f525d8338af'
 target_xml_dir = './target_xml'
-# 原模型： 
+# 原模型: 
 COMPARE_AI_TASK_ID='e9e35528-71c7-4b62-9b88-8970bee55ab6'
 compare_xml_dir = './compare_xml'
+
+# 跳过推理，直接输出上一次结果  True-跳过  False-不跳过推理
+SKIP_PREDICT=False
+
 def check_ai_models(img_dir,target_ai_task_id,compare_ai_task_id):
-    # emptyOutDir('./target_xml')
-    print('清空目录：' + target_xml_dir)
-    # emptyOutDir('./compare_xml')
-    print('清空目录：' + compare_xml_dir)
+    emptyOutDir('./target_xml')
+    print('清空目录:' + target_xml_dir)
+    emptyOutDir('./compare_xml')
+    print('清空目录:' + compare_xml_dir)
 
-    # 推理目标模型
+    if SKIP_PREDICT != True:
+        print('跳过推理目标模型')
+        # 推理目标模型
+        AI_PREDICT.setAiTaskId(target_ai_task_id)
+        AI_PREDICT.ai_predict(img_dir,target_xml_dir,True)
 
-    AI_PREDICT.setAiTaskId(target_ai_task_id)
-    AI_PREDICT.ai_predict(img_dir,target_xml_dir,True)
-
-    # 推理对比模型
-
-    AI_PREDICT.setAiTaskId(compare_ai_task_id)
-    AI_PREDICT.ai_predict(img_dir,compare_xml_dir,True)
+        # 推理对比模型
+        AI_PREDICT.setAiTaskId(compare_ai_task_id)
+        AI_PREDICT.ai_predict(img_dir,compare_xml_dir,True)
 
     # 输入文件总数
     img_count = 0
@@ -54,22 +58,36 @@ def check_ai_models(img_dir,target_ai_task_id,compare_ai_task_id):
         for file in files:
             if file.endswith('.xml'):
                 compare_xml_count += 1
-    print("----  检测结果  ----")
-    print(f'测试图片总数：{ img_count } - 全部为包含目标检测内容的测试集')
-    print(f'目标模型检测推理结果 (目标模型识别/总数)： { target_xml_count }/{ img_count }   目标模型识别率：{ round(target_xml_count / img_count * 100,2) }%')
-    print(f'对比模型检测推理结果 (对比模型识别/总数)： { compare_xml_count }/{ img_count}   对比模型识别率：{ round(compare_xml_count / img_count * 100,2) }%')
-    print(f'相同识别结果（对比模型相同识别数/目标模型识别数）： { same_xml_count }/{ target_xml_count }')
-    print(f'目标与对比模型重复识别率： { round((same_xml_count / target_xml_count)* 100,2) }%')
-    print(f'指标增长个数 (新增识别/识别总数)： { target_xml_count - same_xml_count} / { target_xml_count }' )
-    print(f'目标识别率提升： { round((target_xml_count - same_xml_count) / target_xml_count* 100,2) }%' )
+    result = "----  检测结果  ----\n"
+    result += f'测试图片总数:{ img_count } - 全部为包含目标检测内容的测试集\n'
+    result += f'目标模型ID: {TARGET_AI_TASK_ID}\n'
+    result += f'对比模型ID: {COMPARE_AI_TASK_ID}\n'
+    result += f'目标模型识别结果 (目标模型识别/图片总数): { target_xml_count }/{ img_count }   目标模型识别率:{ numDividedFormat(target_xml_count , img_count) }\n'
+    result += f'对比模型识别结果 (对比模型识别/图片总数): { compare_xml_count }/{ img_count}   对比模型识别率:{ numDividedFormat(compare_xml_count , img_count) }\n'
+    result += f'对比模型相同识别结果（对比模型相同识别数/目标模型识别数）: { same_xml_count }/{ target_xml_count }\n'
+    result += f'目标与对比模型再次识别率: { numDividedFormat(same_xml_count , compare_xml_count) }\n'
+    result += f'模板模型识别增长 (新增识别/识别总数): { target_xml_count - same_xml_count} / { target_xml_count }\n'
+    result += f'目标识别率提升: { numDividedFormat((target_xml_count - same_xml_count) , same_xml_count) }\n'
+    print(result)
 
+    # 写入文件
+    with open("./result.log", 'w', encoding='utf-8') as txt_file:
+        txt_file.write(result)
+        print("对比结果文件生成成功(./result.log)")
 
+# 除法，分母为0时返回0
+def numDividedFormat(a,b):
+    if b == 0:
+        return '0%'
+    return f'{round(a/b * 100,2)}%'
 
 # 清空输出目录
 def emptyOutDir(out_dir): 
     if os.path.exists(out_dir):
-        os.remove(out_dir)
-        os.makedirs(out_dir)
+        # os.remove(out_dir)
+        for root, dirs, files in os.walk(out_dir):
+            for file in files:
+                os.remove(os.path.join(root, file))
     else:
         os.makedirs(out_dir)
 
